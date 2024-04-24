2a92ed3d425606d04267f8f13d48906373eb3eba
Chase <500198871@fvtc.edu>
2024-04-23 19:07:30 -0500

    db test

---
+++ app.js
@@ -0,0 +1,16 @@
+const {MongoClient, ObjectId} = require('mongodb')
+const {url} = process.env.MONGODB_URL || require('../secrets/mongodb.json')
+const client = new MongoClient(url)
+
+const getCollection = async (dbName, collectionName) => {
+    await client.connect()
+    return client.db(dbName).collection(collectionName) 
+}
+(async () => {
+    const collection1 = await getCollection('WheelyGoodBBQ','events')
+    const collection2 = await getCollection('WheelyGoodBBQ','menu')
+    const menus = await collection2.find().toArray()
+    const events = await collection1.find().toArray()
+    console.log(menus)
+    console.log(events)
+})()
