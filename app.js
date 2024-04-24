71958fd876c5e6ff506d847aa24bde62f217fa3c
Chase Handrich <500198871@fvtc.edu>
2024-04-23 21:43:17 -0500

    added more api gets and some basic nav to the web pages

--- app.js
+++ app.js
@@ -6,9 +6,26 @@ const port = process.env.PORT || 3000
 const root = path.join(__dirname,'public')
 
 app.use(express.json())
+app.use(express.static('public'))
 app.use('/api',require('./routes/public'))
 
+app.get('/',(_,response)=>{
+    response.sendFile('Home/home.html',{root})
+})
+app.get('/menu',(_,response)=>{
+    response.sendFile('Menu/menu.html',{root})
+})
+app.get('/contact',(_,response)=>{
+    response.sendFile('Contact/contact.html',{root})
+})
+app.get('/admin',(_,response)=>{
+    response.sendFile('Admin/admin.html',{root})
+})
 
+
+app.get('*',(_,response)=>{
+    response.sendFile('404.html',{root})
+})
 
 const message = `Server running: http://localhost:${port}`
 app.listen(port, () => console.log(message))
