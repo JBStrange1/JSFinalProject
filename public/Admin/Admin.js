d28e59cfec9fc4a8a0817c9ff253a9c74c1fbbcd
Chase Handrich <500198871@fvtc.edu>
2024-04-30 15:08:21 -0500

    stuff

--- public/Admin/Admin.js
+++ public/Admin/Admin.js
@@ -15,12 +15,15 @@
 		radio.type = 'radio'
 		radio.checked = true
         radio.name= 'events'
-        radio.id = ''
+        radio.id = 'empty'
 		radio.addEventListener('change', () => {
 			document.querySelector('.eventName').value = ""
             document.querySelector('.eventDate').value = ""
             document.querySelector('.truckLocation').value = ""
             document.querySelector('.hours').value = ""
+
+            document.querySelector('#update_create').value = "Create"
+            document.querySelector('#delete').hidden = true
 		})
 		li.appendChild(radio)
 
@@ -28,20 +31,24 @@
 			const li = document.createElement('li')
 			eventsList.appendChild(li)
 
-			const span = document.createElement('span')
-			span.textContent = eventName + ', '+ truckLocation +', ' + eventDate + ', ' + hours
-			li.appendChild(span)
+			const label = document.createElement('label')
+			label.textContent = eventName + ', '+ truckLocation +', ' + eventDate + ', ' + hours
+            label.htmlFor= _id
+            li.appendChild(label)
 
 			const radio = document.createElement('input')
 			radio.type = 'radio'
 			radio.checked = false
             radio.name= 'events'
             radio.id = _id
+            radio.value = _id
 			radio.addEventListener('change', () => {
 				document.querySelector('.eventName').value = eventName
                 document.querySelector('.eventDate').value = eventDate
                 document.querySelector('.truckLocation').value = truckLocation
                 document.querySelector('.hours').value = hours
+                document.querySelector('#update_create').value = "Update"
+                document.querySelector('#delete').hidden = false
 			})
 			li.appendChild(radio)
 		})
@@ -49,7 +56,16 @@
 
 
     displayEvents(await getEvents())
-    document.querySelector('.update_create').addEventListener('submit',() =>{
+    document.querySelector('#update_create').addEventListener('submit',() =>{
+        SubmitEvent.preventDefault()
+        var form = document.getElementById("eventsForm");
+        var formData = new FormData(form);
+        for (var [key, value] of formData.entries()) {
+            // Process each input name and value
+            console.log(key + ": " + value);
+        }
+    })
+    document.querySelector('#delete').addEventListener('submit',() =>{
         
     })
 })()
