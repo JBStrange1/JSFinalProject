c7d0b09ee459f7dbacb747cd8b75f7f8fcbb937c
John Strange <300089261@fvtc.edu>
2024-04-30 23:30:34 -0500

    Completed some endpoints still needs testing.

--- routes/public.js
+++ routes/public.js
@@ -16,6 +16,29 @@ route.get('/menu', async (_,response)=>{
     response.json(items)
 })
 
+route.post('/menu'), async ( request , response) => {
+    const { body } = request
+    const { name, description, price } = body
+    const item = { name, description, price }
+    const collection = await getCollection('WheelyGoodBBQ', 'menu')
+    const result = await collection.insertOne(item)
+    response.json(result)
+}
+route.put('/menu/:id'), async ( request , response) => {
+    const { body, params } = request
+    const { id } = params
+    const { name, description, price } = body
+    const item = { name, description, price }
+    const collection = await getCollection('WheelyGoodBBQ', 'menu')
+    const result = await collection.updateOne( { _id: new ObjectId(id) }, {$set: item})
+    response.json(result)
+}
+route.delete('/menu/:id'), async (request, response)  => {
+    const { id } = request.params
+    const collection = await getCollection('WheelyGoodBBQ', 'menu')
+    const result = await collection.deleteOne({_id: new ObjectId(id)})
+    response.json(result)
+}
 
 route.get('/events', async (_,response)=>{
     const collection = await getCollection('WheelyGoodBBQ','events')
@@ -24,12 +47,20 @@ route.get('/events', async (_,response)=>{
 })
 
 
-
 route.get('/events/:id', async (request,response)=>{
     const {id} = request.params
     const collection = await getCollection('WheelyGoodBBQ','events')
     const event = await collection.findOne({_id:new ObjectId(id)})
     response.json(event)
 })
+
+route.post('/events'), async ( request , response) => {
+    const { body } = request
+    const { name, location, dates, hours } = body
+    const event = { name, location, dates, hours }
+    const collection = await getCollection('WheelyGoodBBQ', 'menu')
+    const result = await collection.insertOne(event)
+    response.json(result)
+}
 
 module.exports = route
