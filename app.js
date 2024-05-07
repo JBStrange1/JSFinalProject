923f3c19de69262c8df975b25333cb273f770a8f
JBStrange1 <143125442+JBStrange1@users.noreply.github.com>
2024-05-07 13:27:33 -0500

    Did nothing dont know why i have changes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const {MongoClient, ObjectId} = require('mongodb')
-const {url} = process.env.MONGODB_URL || require('../secrets/mongodb.json')
+const { url } = process.env.MONGODB_URL || require('./secrets/mongodb.json')
 const client = new MongoClient(url)
-
 const getCollection = async (dbName, collectionName) => {
     await client.connect()
     return client.db(dbName).collection(collectionName) 
