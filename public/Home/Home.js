1eef5736765d0c05e7f4ee0757aeed676024b1ba
John Strange <300089261@fvtc.edu>
2024-05-09 21:35:07 -0500

    Im not gonna lie idk what i did

--- public/Home/Home.js
+++ public/Home/Home.js
@@ -1,12 +1,48 @@
+(async () => {
+    const getEvents = async () => {
+        const response = await fetch('/api/events')
+        const events = await response.json()
+        return events
+    }
 
-const cardInfo1 = {
-   
-}
+    const displayEvents = async () => {
+        const events = await getEvents()
+        const cardsContainer = document.getElementById('cards')
 
-const cardInfo2 = {
+        events.forEach(event => {
+            const card = document.createElement('div')
+            card.classList.add('card')
+            card.innerHTML = `
+                <div>
+                    <h1>${event.name}</h1>
+                    <p class="cardText">Location: ${event.location}</p>
+                    <p class="cardText">Dates: ${event.dates}</p>
+                    <p class="cardText">Hours: ${event.hours}</p>
+                </div>
+            `
+            cardsContainer.appendChild(card)
+        })
+    }
 
-}
+    await displayEvents()
 
-const cardInfo3 = {
+    const previousButton = document.querySelector('.previous')
+    const nextButton = document.querySelector('.next')
+    const cardsContainer = document.getElementById('cards')
+    let currentIndex = 0
 
-}
+    previousButton.addEventListener('click', () => {
+        if (currentIndex > 0) {
+            currentIndex--
+            cardsContainer.style.transform = `translateX(-${currentIndex * 100}%)`
+        }
+    })
+
+    nextButton.addEventListener('click', () => {
+        const numCards = cardsContainer.querySelectorAll('.card').length
+        if (currentIndex < numCards - 1) {
+            currentIndex++
+            cardsContainer.style.transform = `translateX(-${currentIndex * 100}%)`
+        }
+    })
+})()
