9482e91335930ac6d3ef90ff90500aa004798954
JBStrange1 <143125442+JBStrange1@users.noreply.github.com>
2024-05-14 11:30:20 -0500

    Finished up the menu with data from mongo db

--- public/Menu/Menu.js
+++ public/Menu/Menu.js
@@ -6,6 +6,48 @@
         return menu;
     };
 
+    const displayMenu = async () => {
+        const menu = await getMenu()
+        const menuContainer = document.getElementById('menuContainer')
 
+        menu.forEach(item => {
+            
+            const { name, description, price, imagePath } = item; 
+            
+            const menuItemDiv = document.createElement('div')
+            menuItemDiv.classList.add('menuItem')
+
+            const imageContainer = document.createElement('div')
+            imageContainer.classList.add('imageContainer')
+
+            const image = document.createElement('img')
+            image.classList.add('menuImage')
+            image.src = imagePath
+
+            const itemDescriptionDiv = document.createElement('div')
+            itemDescriptionDiv.classList.add('itemDescription')
+    
+            const itemName = document.createElement('p')
+            itemName.classList.add('name')
+            itemName.textContent = name
+    
+            const itemDescription = document.createElement('p')
+            itemDescription.classList.add('description')
+            itemDescription.textContent = description
+    
+            const itemPrice = document.createElement('p')
+            itemPrice.classList.add('price')
+            itemPrice.textContent = price
+    
+            imageContainer.appendChild(image);
+            itemDescriptionDiv.appendChild(itemName);
+            itemDescriptionDiv.appendChild(itemDescription);
+            itemDescriptionDiv.appendChild(itemPrice);
+
+            menuItemDiv.appendChild(imageContainer);
+            menuItemDiv.appendChild(itemDescriptionDiv);
+            menuContainer.appendChild(menuItemDiv);
+        })
+    }
     await displayMenu();
 })();
